d0a87f6a8eb53c1e9f2ee5e18e310a72906cb7d3
yoocho1103 <whalsgur0000@naver.com>
2020-05-26 15:04:05 +0900

    Food scrollbar 수정
    Food scrollbar 수정하고 반응형전까지 완료
    메뉴와 반응형 작업해야함

--- jquery/main.js
+++ jquery/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     $('section.foodlist div.foodlist_wrap span').click(function(){
         $('section.foodlist div.wrap ul li > div').removeClass('foodList_overlay');
         $('section.foodlist div > span').removeClass('opcity_1');
-    });
+    });//what people say  open /close
     
     $('section.foodCategory span').click(function(){
         if($('section.foodCategory div').hasClass('minusCategory')){
@@ -16,7 +16,16 @@ $(document).ready(function(){
             $('section.foodCategory div').removeClass('plusCategory');
             $('section.foodCategory div').addClass('minusCategory');
         }
+    }); //top10 open /close
+
+    $('section.foodCategory span:first-of-type').click(function(){
+        $(this).addClass('nonSpan');
+        $('section.foodCategory span:last-of-type').removeClass('nonSpan');
     });
+    $('section.foodCategory span:last-of-type').click(function(){
+        $('section.foodCategory span:first-of-type').removeClass('nonSpan');
+        $(this).addClass('nonSpan');
+    });//더보기
 
     $('section.map div div').click(function(){
         if($('section.map div iframe').hasClass('map_open')){
@@ -28,7 +37,7 @@ $(document).ready(function(){
             $('section.map div div h2:nth-of-type(1)').addClass('text_delete');
             $('section.map div div h2:nth-of-type(2)').removeClass('text_delete');
         }
-    });
+    }); //지도 open close
 
     $('section.chef ul li:nth-of-type(1) div').click(function(){
         $('section.chef ul li div').removeClass('chef_introduce');
@@ -52,15 +61,35 @@ $(document).ready(function(){
         $('section.chef ul li:nth-of-type(3)').addClass('chef_margin'); //margin으로 밀기
         $(this).addClass('chef_introduce');//카드 오픈
     });
+    // $('section.chef ul li div').click(function(){
+    //     if($('section.chef ul li div').hasClass('chef_introduce')){
+    //         //$(this).removeClass('chef_introduce');
+    //         alert('');
+    //     }
+    // });
     /*chef 카드 열기 */
-    $(document).scroll(function(){
-        console.log($(document).scrollTop());
-        console.log($('section.introduce').offset().top); 
-
+    $('.arrow_top').click(function(){
+        $('html, body').animate({scrollTop:0}, '500', 'swing');
+    }) //맨위로 가기
+    $(document).scroll(function(){    
+        if($(document).scrollTop() > $('.foodCategory').offset().top-800){
+            $('section.foodCategory > ul:first-of-type li').removeClass('positionType');
+            $('section.foodCategory > ul:first-of-type li:nth-of-type(1)').addClass('scroll1');
+            $('section.foodCategory > ul:first-of-type li:nth-of-type(2)').addClass('scroll2');
+            $('section.foodCategory > ul:first-of-type li:nth-of-type(3)').addClass('scroll3');
+            $('section.foodCategory > ul:first-of-type li:nth-of-type(4)').addClass('scroll4');
+        }
+        else if($(document).scrollTop() < $('.foodCategory').offset().top-800){
+            $('section.foodCategory > ul:first-of-type li').addClass('positionType');
+            $('section.foodCategory > ul:first-of-type li:nth-of-type(1)').removeClass('scroll1');
+            $('section.foodCategory > ul:first-of-type li:nth-of-type(2)').removeClass('scroll2');
+            $('section.foodCategory > ul:first-of-type li:nth-of-type(3)').removeClass('scroll3');
+            $('section.foodCategory > ul:first-of-type li:nth-of-type(4)').removeClass('scroll4');
+        }
         var maxScollValue =$(document).height() - $(window).height();
         var scrollPer = $(document).scrollTop() / maxScollValue;
         $('.progressBar div').css('width', (scrollPer * 100) + '%'); //맨위의 스크롤바
-    })
+    })//scroll
 
     $('header div.slider').bxSlider({
         randomStart : true,
@@ -70,5 +99,12 @@ $(document).ready(function(){
         controls: false, // 이전 다음 버튼 노출 여부
         pager : false, //페이지 표시
         adaptiveHeight:true
+    });//bx slider
+
+    $('footer div.footer_top ul li i').mouseenter(function(){
+        $(this).addClass('orange')
+    });
+    $('footer div.footer_top ul li i').mouseleave(function(){
+        $(this).removeClass('orange')
     });
 });
